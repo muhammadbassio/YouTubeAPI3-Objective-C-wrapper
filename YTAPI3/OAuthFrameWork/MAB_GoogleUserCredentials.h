5224d1dd6f6fef9b93b2ead28f6364df6a34c9cd
muhammadbassio <muhammadbassio@Muhammads-MacBook-Pro.local>
2014-06-03 21:21:04 +0300

    Merge all configurations in credentials

--- YTAPI3/OAuthFrameWork/MAB_GoogleUserCredentials.h
+++ YTAPI3/OAuthFrameWork/MAB_GoogleUserCredentials.h
@@ -11,6 +11,26 @@
 #import "MABYT3_Channel.h"
 #import "MABYT3_Subscription.h"
 
+
+#define IsPad      (UI_USER_INTERFACE_IDIOM() != UIUserInterfaceIdiomPhone)
+
+#define UIColorFromRGBA(rgbValue, alphaValue) [UIColor colorWithRed:((CGFloat)((rgbValue & 0xFF0000) >> 16))/255.0 green:((CGFloat)((rgbValue & 0xFF00) >> 8))/255.0 blue:((CGFloat)(rgbValue & 0xFF))/255.0 alpha:alphaValue]
+
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
+#pragma message "You have to register your app at https://console.developers.google.com/ to get ClientID, Client secret & apiKey"
+
+static NSString *kMyClientID = @"___";
+static NSString *kMyClientSecret = @"___";
+static NSString *apiKey = @"___";
+static NSString *scope = @"https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtubepartner https://www.googleapis.com/auth/youtubepartner-channel-audit https://www.googleapis.com/auth/youtube.upload";
+
+
+
 @protocol MAB_GoogleUserCredentialsDelegate <NSObject>
 
 - (void) CredUserNoChannel;
